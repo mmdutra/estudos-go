e36ee92828cf5c0ca99f77f8853e21a168ca1950
Mateus Morais Dutra <mateus.dutra@picpay.com>
2022-07-23 09:10:25 -0300

    adicionando uma dependencia externa!

--- fundamentos/pacotes/main.go
+++ fundamentos/pacotes/main.go
@@ -3,9 +3,20 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo do arquivo main")
 	auxiliar.Escrever()
+
+	validarEmail("teste@teste.com")
+	validarEmail("teste")
+}
+
+func validarEmail(email string) {
+	erro := checkmail.ValidateFormat(email)
+
+	fmt.Println(erro)
 }
