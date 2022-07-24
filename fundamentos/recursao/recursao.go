a0ec267cbf51ef8cc7328fe510024223737f99ed
Mateus Morais Dutra <mateus.dutra@picpay.com>
2022-07-24 17:16:25 -0300

    recursao :fire:

---
+++ fundamentos/recursao/recursao.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func fibonacci(posicao uint) uint {
+	if posicao <= 1 {
+		return posicao
+	}
+
+	return fibonacci(posicao-2) + fibonacci(posicao-1)
+}
+
+func main() {
+	posicao := uint(3)
+
+	fmt.Println(fibonacci(posicao))
+}
