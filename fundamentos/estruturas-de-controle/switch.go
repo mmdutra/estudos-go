53f9320bb67ceaddbdf0d91c058de31eafb88b67
Mateus Morais Dutra <mateus.dutra@picpay.com>
2022-07-24 16:21:55 -0300

    switch :alien:

---
+++ fundamentos/estruturas-de-controle/switch.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+)
+
+func diaDaSemana(numero int) string {
+	switch numero {
+	case 1:
+		return "Domingo"
+	case 2:
+		return "Segunda-feira"
+	case 3:
+		return "Terça-feira"
+	case 4:
+		return "Quarta-feira"
+	case 5:
+		return "Quinta-feira"
+	case 6:
+		return "Sexta-feira"
+	case 7:
+		return "Sábado"
+	default:
+		return "Dia não existente"
+	}
+
+}
+
+func diaDaSemana2(numero int) string {
+	switch {
+	case numero == 1:
+		return "Domingo"
+		fallthrough
+	case numero == 2:
+		return "Segunda-feira"
+	case numero == 3:
+		return "Terça-feira"
+	case numero == 4:
+		return "Quarta-feira"
+	case numero == 5:
+		return "Quinta-feira"
+	case numero == 6:
+		return "Sexta-feira"
+	case numero == 7:
+		return "Sábado"
+	default:
+		return "Dia não existente"
+	}
+
+}
+
+func main() {
+	fmt.Println(diaDaSemana(3))
+	fmt.Println(diaDaSemana(34))
+}
